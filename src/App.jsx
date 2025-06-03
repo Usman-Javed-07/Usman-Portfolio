4b13edef27107ee27e59a777b4365fe3e121f4cb
Usman Javed <usmanjaved0816@gmail.com>
2025-06-03 18:37:42 +0500

    add navbar list items

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,30 @@
 import "./App.css";
-
+import { FaWhatsapp } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+import { FaGithub } from "react-icons/fa";
+import { FaLinkedin } from "react-icons/fa";
+import { FaLongArrowAltRight } from "react-icons/fa";
 function App() {
   return (
     <>
-      <h1>hello</h1>
+      {/* navbar */}
+      <header className="navbar">
+        <nav className="navbar-container">
+          <div className="navbar-logo">
+            <a href="#"> <img className="logo-image" src="./images/logo.png" alt="logo image" /></a>
+          </div>
+          <ul className="navbar-links">
+            <li><a href="#"><FaWhatsapp /></a></li>
+            <li><a href="#"><MdEmail /></a></li>
+            <li><a href="#"><FaGithub /></a></li>
+            <li><a href="#"><FaLinkedin /></a></li>
+            <li className="resume-link"><a className="resume" href="#">Resume <span className="arrow"><FaLongArrowAltRight /></span> </a></li>
+            
+           
+          </ul>
+        </nav>
+
+      </header>
     </>
   );
 }
