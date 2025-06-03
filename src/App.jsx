94f634ff9facde4763f4cdeacadcbe219387b7a5
Usman Javed <usmanjaved0816@gmail.com>
2025-06-03 18:51:53 +0500

    make navbar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,37 @@
+import { useState } from "react";
 import "./App.css";
-import { FaWhatsapp } from "react-icons/fa";
+import { FaWhatsapp, FaGithub, FaLinkedin, FaLongArrowAltRight, FaBars, FaTimes } from "react-icons/fa";
 import { MdEmail } from "react-icons/md";
-import { FaGithub } from "react-icons/fa";
-import { FaLinkedin } from "react-icons/fa";
-import { FaLongArrowAltRight } from "react-icons/fa";
+
 function App() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
   return (
     <>
-      {/* navbar */}
       <header className="navbar">
         <nav className="navbar-container">
           <div className="navbar-logo">
-            <a href="#"> <img className="logo-image" src="./images/logo.png" alt="logo image" /></a>
+            <a href="#">
+              <img className="logo-image" src="./images/logo.png" alt="logo image" />
+            </a>
           </div>
-          <ul className="navbar-links">
+
+          <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </div>
+
+          <ul className={`navbar-links ${menuOpen ? "active" : ""}`}>
             <li><a href="#"><FaWhatsapp /></a></li>
             <li><a href="#"><MdEmail /></a></li>
             <li><a href="#"><FaGithub /></a></li>
             <li><a href="#"><FaLinkedin /></a></li>
-            <li className="resume-link"><a className="resume" href="#">Resume <span className="arrow"><FaLongArrowAltRight /></span> </a></li>
-            
-           
+            <li className="resume-link">
+              <a className="resume" href="#">
+                Resume <span className="arrow"><FaLongArrowAltRight /></span>
+              </a>
+            </li>
           </ul>
         </nav>
-
       </header>
     </>
   );
