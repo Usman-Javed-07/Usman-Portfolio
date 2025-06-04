0d8dc90c5b143affb88f31a3e04da55761df4eb5
Usman Javed <usmanjaved0816@gmail.com>
2025-06-04 22:05:28 +0500

    formate document

--- src/App.jsx
+++ src/App.jsx
@@ -20,82 +20,86 @@ function App() {
   return (
     <>
       <header className="navbar">
+        <nav className="navbar-container">
+          <div className="navbar-logo">
+            <a href="#">
+              <img
+                className="logo-image"
+                src="./images/logo.png"
+                alt="logo image"
+              />
+            </a>
+          </div>
 
-       <nav className="navbar-container">
-  <div className="navbar-logo">
-    <a href="#">
-      <img
-        className="logo-image"
-        src="./images/logo.png"
-        alt="logo image"
-      />
-    </a>
-  </div>
+          <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+            {menuOpen ? <FaTimes /> : <FaBars />}
+          </div>
 
-  <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
-    {menuOpen ? <FaTimes /> : <FaBars />}
-  </div>
-
-  <ul className={`navbar-links ${menuOpen ? "active" : ""}`}>
-    <li>
-      <a
-        href="https://wa.me/+923415477580"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FaWhatsapp />
-      </a>
-    </li>
-    <li>
-      <a
-        href="mailto:usmanjaved0816@gmail.com"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <MdEmail />
-      </a>
-    </li>
-    <li>
-      <a
-        href="https://github.com/Usman-Javed-07"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FaGithub />
-      </a>
-    </li>
-    <li>
-      <a
-        href="https://www.linkedin.com/in/usman-javed07/"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        <FaLinkedin />
-      </a>
-    </li>
-    <li className="resume-link">
-      <a
-        className="resume"
-        href="/Usman javed(web developer).pdf"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        Resume <span className="arrow"><FaLongArrowAltRight /></span>
-      </a>
-    </li>
-  </ul>
-</nav>
-
-
+          <ul className={`navbar-links ${menuOpen ? "active" : ""}`}>
+            <li>
+              <a
+                href="https://wa.me/+923415477580"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaWhatsapp />
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:usmanjaved0816@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <MdEmail />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/Usman-Javed-07"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/usman-javed07/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin />
+              </a>
+            </li>
+            <li className="resume-link">
+              <a
+                className="resume"
+                href="/Usman javed(web developer).pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Resume{" "}
+                <span className="arrow">
+                  <FaLongArrowAltRight />
+                </span>
+              </a>
+            </li>
+          </ul>
+        </nav>
         <section className="hero">
           <div className="hero-content">
             <p className="header-para">
               Hello <span className="wave">👋</span> I'm
             </p>
             <span className="header-heading">
-              <span className="code-icon"><FaLessThan /></span>
+              <span className="code-icon">
+                <FaLessThan />
+              </span>
               Usman <br /> Javed/
-              <span className="code-icon"><FaGreaterThan /></span>
+              <span className="code-icon">
+                <FaGreaterThan />
+              </span>
             </span>
             <p className="header-para">
               Full Stack <Typewriter text="Web Developer" />
