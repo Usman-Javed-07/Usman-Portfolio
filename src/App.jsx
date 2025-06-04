0ad2629767fdbc9f166f1963093832731347e032
Usman Javed <usmanjaved0816@gmail.com>
2025-06-04 22:04:58 +0500

    add whatsapp linkdein github email and resume link

--- src/App.jsx
+++ src/App.jsx
@@ -20,49 +20,72 @@ function App() {
   return (
     <>
       <header className="navbar">
-        <nav className="navbar-container">
-          <div className="navbar-logo">
-            <a href="#">
-              <img
-                className="logo-image"
-                src="./images/logo.png"
-                alt="logo image"
-              />
-            </a>
-          </div>
 
-          <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
-            {menuOpen ? <FaTimes /> : <FaBars />}
-          </div>
+       <nav className="navbar-container">
+  <div className="navbar-logo">
+    <a href="#">
+      <img
+        className="logo-image"
+        src="./images/logo.png"
+        alt="logo image"
+      />
+    </a>
+  </div>
 
-          <ul className={`navbar-links ${menuOpen ? "active" : ""}`}>
-            <li>
-              <a href="#">
-                <FaWhatsapp />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <MdEmail />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <FaGithub />
-              </a>
-            </li>
-            <li>
-              <a href="#">
-                <FaLinkedin />
-              </a>
-            </li>
-            <li className="resume-link">
-              <a className="resume" href="#">
-                Resume <span className="arrow"><FaLongArrowAltRight /></span>
-              </a>
-            </li>
-          </ul>
-        </nav>
+  <div className="menu-toggle" onClick={() => setMenuOpen(!menuOpen)}>
+    {menuOpen ? <FaTimes /> : <FaBars />}
+  </div>
+
+  <ul className={`navbar-links ${menuOpen ? "active" : ""}`}>
+    <li>
+      <a
+        href="https://wa.me/+923415477580"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaWhatsapp />
+      </a>
+    </li>
+    <li>
+      <a
+        href="mailto:usmanjaved0816@gmail.com"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <MdEmail />
+      </a>
+    </li>
+    <li>
+      <a
+        href="https://github.com/Usman-Javed-07"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaGithub />
+      </a>
+    </li>
+    <li>
+      <a
+        href="https://www.linkedin.com/in/usman-javed07/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <FaLinkedin />
+      </a>
+    </li>
+    <li className="resume-link">
+      <a
+        className="resume"
+        href="/Usman javed(web developer).pdf"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Resume <span className="arrow"><FaLongArrowAltRight /></span>
+      </a>
+    </li>
+  </ul>
+</nav>
+
 
         <section className="hero">
           <div className="hero-content">
