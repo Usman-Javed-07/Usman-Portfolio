d8122b7990f90b782ea82f2a7c2e4a5d4659d98d
Usman Javed <usmanjaved0816@gmail.com>
2025-06-05 17:03:54 +0500

    add about section

--- src/App.jsx
+++ src/App.jsx
@@ -87,7 +87,6 @@ function App() {
               </a>
             </li>
           </ul>
-
         </nav>
         <section className="hero">
           <div className="hero-content">
@@ -109,8 +108,8 @@ function App() {
           </div>
         </section>
 
-          <div className="sidebar-icons">
-             <ul className={"sidebar-links"}>
+        <div className="sidebar-icons">
+          <ul className={"sidebar-links"}>
             <li>
               <a
                 href="https://wa.me/+923415477580"
@@ -148,30 +147,110 @@ function App() {
               </a>
             </li>
             <li>
-              <span className="icon-bar"><PiLineVerticalThin /></span>
+              <span className="icon-bar">
+                <PiLineVerticalThin />
+              </span>
             </li>
           </ul>
-          </div>
+        </div>
 
-          <div className="my-image">
-           <img src="./images/usman.png" alt="my image" />
-          </div>
-               <div className="node-icon">
-                <img src="./images/nod.png" alt="node js" />
-               </div>
+        <div className="my-image">
+          <img src="./images/usman.png" alt="my image" />
+        </div>
+        <div className="node-icon">
+          <img src="./images/nod.png" alt="node js" />
+        </div>
 
-               <div className="js-icon">
-                <img src="./images/js.png" alt="js" />
-               </div>
+        <div className="js-icon">
+          <img src="./images/js.png" alt="js" />
+        </div>
 
-               <div className="react-icon">
-                <img src="./images/react.png" alt="react" />
-               </div>
+        <div className="react-icon">
+          <img src="./images/react.png" alt="react" />
+        </div>
 
-               <div className="html-icon">
-                <img src="./images/html.png" alt="html" />
-               </div>
+        <div className="html-icon">
+          <img src="./images/html.png" alt="html" />
+        </div>
       </header>
+
+      {/* Main Content */}
+      <main className="main-content">
+        <section className="about">
+          <h1 className="about-heading">
+            {" "}
+            <span className="text-about">About</span> ( ) {"{"}{" "}
+          </h1>
+          <p className="about-para">
+            I'm a passionate Full Stack Developer skilled in building modern,
+            responsive, and user-friendly web applications using JavaScript and
+            React.js. I have a strong grasp of component-based architecture,
+            hooks, state management, and routing. On the backend, I work with
+            Node.js and Express to create clean and efficient RESTful APIs. I’m
+            confident in integrating third-party APIs and developing dynamic,
+            data-driven features. With experience in Git, GitHub, and modern
+            development tools, I thrive in fast-paced team environments and
+            always aim to deliver high-quality, on-time results.
+          </p>
+          <p className="aboutwork-para">#open_to_work #open_to_relocation</p>
+           <div className="resume-link more-about-me">
+              <a
+                className="resume"
+                href="/Usman javed(web developer).pdf"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                    more about me{" "}
+                <span className="arrow">
+                  <FaLongArrowAltRight />
+                </span>
+              </a>
+            </div>
+          <div className="my-accounts">
+          <ul className={"account-links"}>
+            <li>
+              <a
+                href="https://wa.me/+923415477580"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaWhatsapp />
+              </a>
+            </li>
+            <li>
+              <a
+                href="mailto:usmanjaved0816@gmail.com"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <MdEmail />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://github.com/Usman-Javed-07"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaGithub />
+              </a>
+            </li>
+            <li>
+              <a
+                href="https://www.linkedin.com/in/usman-javed07/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FaLinkedin />
+              </a>
+            </li>
+  
+          </ul>
+        </div> <br /> <br />
+           <h1 className="about-heading">{"}"}{" "}</h1>
+
+        </section>
+      </main>
     </>
   );
 }
