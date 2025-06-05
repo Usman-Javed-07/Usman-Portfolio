82223d3b17bca9350df2e72d35b04e97e2461d60
Usman Javed <usmanjaved0816@gmail.com>
2025-06-05 15:08:34 +0500

    add react icon

--- src/App.jsx
+++ src/App.jsx
@@ -163,6 +163,9 @@ function App() {
                <div className="js-icon">
                 <img src="./images/js.png" alt="js" />
                </div>
+               <div className="react-icon">
+                <img src="./images/react.png" alt="react" />
+               </div>
       </header>
     </>
   );
