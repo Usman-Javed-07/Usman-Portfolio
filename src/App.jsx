a66ca453662da475c70884772993bbce57391bc0
Usman Javed <usmanjaved0816@gmail.com>
2025-06-16 14:06:11 +0500

    complete projet sections

--- src/App.jsx
+++ src/App.jsx
@@ -17,143 +17,166 @@ import Typewriter from "./Typewriter.jsx";
 
 const projectsData = [
   {
-    image: './images/solslogo.png',
-    title: 'Cara E-commerce website',
-    link: 'https://github.com/Usman-Javed-07/Html-Css-design-6',
-    displayLink: 'caraEcom.com',
+    image: "./images/solslogo.png",
+    title: "Cara E-commerce website",
+    link: "https://github.com/Usman-Javed-07/Html-Css-design-6",
+    displayLink: "caraEcom.com",
     description:
-      'Cara is a modern online shopping platform specializing in fashionable clothing. Customers can explore a wide range of trendy outfits for men, women, and kids. With a user-friendly interface and secure checkout.',
+      "Cara is a modern online shopping platform specializing in fashionable clothing. Customers can explore a wide range of trendy outfits for men, women, and kids. With a user-friendly interface and secure checkout.",
   },
   {
-    image: './images/zbeelogo.png',
-    title: 'Private Artwork Management',
-    link: 'https://github.com/Usman-Javed-07/Artwork-management-frontend',
-    displayLink: 'Artwork.Management.com',
+    image: "./images/zbeelogo.png",
+    title: "Private Artwork Management",
+    link: "https://github.com/Usman-Javed-07/Artwork-management-frontend",
+    displayLink: "Artwork.Management.com",
     description:
-      'Private Artwork Management is an art-focused website showcasing a diverse collection of unique artworks. It features creative pieces inspired by everyday objects like cups, paper, and floral themes.',
+      "Private Artwork Management is an art-focused website showcasing a diverse collection of unique artworks. It features creative pieces inspired by everyday objects like cups, paper, and floral themes.",
   },
   {
-    image: './images/freelancerlogo.png',
-    title: 'Ai Mental Health Therapist',
-    link: 'https://github.com/Usman-Javed-07/Ai-mental-Health-Frontend',
-    displayLink: 'AiMHT.com',
+    image: "./images/freelancerlogo.png",
+    title: "Ai Mental Health Therapist",
+    link: "https://github.com/Usman-Javed-07/Ai-mental-Health-Frontend",
+    displayLink: "AiMHT.com",
     description:
-      'AI Mental Health Therapist is a virtual assistant that helps you understand and manage stress, anxiety, and depression. It offers quick, personalized support for better mental well-being.',
+      "AI Mental Health Therapist is a virtual assistant that helps you understand and manage stress, anxiety, and depression. It offers quick, personalized support for better mental well-being.",
   },
   {
-    image: './images/zbeelogo.png',
-    title: 'Slot Machine',
-    link: 'https://github.com/Usman-Javed-07/Slot-machine',
-    displayLink: 'slotmachine.com',
+    image: "./images/zbeelogo.png",
+    title: "Slot Machine",
+    link: "https://github.com/Usman-Javed-07/Slot-machine",
+    displayLink: "slotmachine.com",
     description:
-      'Slot Machine is a engaging game that simulates the experience of playing a real slot machine. Spin the reels and try your luck to win exciting prizes and rewards.',
+      "Slot Machine is a engaging game that simulates the experience of playing a real slot machine. Spin the reels and try your luck to win exciting prizes and rewards.",
   },
   {
-    image: './images/zbeelogo.png',
-    title: '4 week Qtr',
-    link: 'https://github.com/Usman-Javed-07/4-week-qtr',
-    displayLink: 'weekQtr.com',
+    image: "./images/zbeelogo.png",
+    title: "4 week Qtr",
+    link: "https://github.com/Usman-Javed-07/4-week-qtr",
+    displayLink: "weekQtr.com",
     description:
-      '4 Week QTR is a personalized routine management project where users can set monthly goals, daily targets, and receive 3 customizable notifications per day at preferred times.',
+      "4 Week QTR is a personalized routine management project where users can set monthly goals, daily targets, and receive 3 customizable notifications per day at preferred times.",
   },
   {
-    image: './images/solslogo.png',
-    title: 'Quizzical App',
-    link: 'https://github.com/Usman-Javed-07/quizzicalApp',
-    displayLink: 'quizzical.com',
+    image: "./images/solslogo.png",
+    title: "Quizzical App",
+    link: "https://github.com/Usman-Javed-07/quizzicalApp",
+    displayLink: "quizzical.com",
     description:
-      'Quizzical is a quiz app with subjects like Computer Science, Music, Animals, Sports, and more. It shows user scores and correct answers at the end, helping users test and improve their knowledge.',
-  },
-    {
-    image: './images/solslogo.png',
-    title: 'Hug or shrug',
-    link: 'https://github.com/Usman-Javed-07/html-css-design-5',
-    displayLink: 'Hug.shrug.com',
-    description:
-      'Hug or Shrug is a fun web app for entertainment where users can share and rate things like brands, lifestyle choices, habits, hobbies, and more. Its a lighthearted way to express opinions and see what others think.'
-  },
-     {
-    image: './images/zbeelogo.png',
-    title: 'XRCHIV',
-    link: 'https://github.com/Usman-Javed-07/Xarchive',
-    displayLink: 'xrchiv.com',
-    description:
-      'XRCHIV is a application built for efficient data management and visualization. The project provides a streamlined user interface with robust features for creating, viewing, and managing data archives.'
-  },
-   {
-    image: './images/zbeelogo.png',
-    title: 'Plate Of Africa',
-    link: 'https://github.com/Usman-Javed-07/Plate-of-Africa',
-    displayLink: 'Plate-Of-Africa.com',
-    description:
-      'Plate of Africa is a restaurant website where users can explore the menu, place orders, and pay online seamlessly. It offers a smooth and convenient dining experience from browsing to checkout.'
+      "Quizzical is a quiz app with subjects like Computer Science, Music, Animals, Sports, and more. It shows user scores and correct answers at the end, helping users test and improve their knowledge.",
   },
   {
-    image: './images/zbeelogo.png',
-    title: 'character image',
-    link: 'https://github.com/Usman-Javed-07/character-image-backend',
-    displayLink: 'character-image.com',
+    image: "./images/solslogo.png",
+    title: "Hug or shrug",
+    link: "https://github.com/Usman-Javed-07/html-css-design-5",
+    displayLink: "Hug.shrug.com",
     description:
-      'Character Image is a Shopify store where users can browse and purchase merchandise featuring their favorite characters. It offers a fun and personalized shopping experience for fans of all kinds.'
+      "Hug or Shrug is a fun web app for entertainment where users can share and rate things like brands, lifestyle choices, habits, hobbies, and more. Its a lighthearted way to express opinions and see what others think.",
   },
-   {
-    image: './images/zbeelogo.png',
-    title: 'atlas-ai',
-    link: 'https://github.com/Usman-Javed-07/atlasAi',
-    displayLink: 'atlas-ai.com',
+  {
+    image: "./images/zbeelogo.png",
+    title: "XRCHIV",
+    link: "https://github.com/Usman-Javed-07/Xarchive",
+    displayLink: "xrchiv.com",
     description:
-      'This application enables users to interact with an AI through text or voice, choose from a variety of AI personas, and seamlessly send emails via the Gmail API.'
+      "XRCHIV is a application built for efficient data management and visualization. The project provides a streamlined user interface with robust features for creating, viewing, and managing data archives.",
   },
-   {
-    image: './images/zbeelogo.png',
-    title: 'Clothing Store',
-    link: 'https://github.com/Usman-Javed-07/clothing-store-backend',
-    displayLink: 'ClothingStore.com',
+  {
+    image: "./images/zbeelogo.png",
+    title: "Plate Of Africa",
+    link: "https://github.com/Usman-Javed-07/Plate-of-Africa",
+    displayLink: "Plate-Of-Africa.com",
     description:
-      'Clothing Store is an e-commerce website where users can browse and purchase apparel online, add items to their cart, and complete secure payments with ease.'
+      "Plate of Africa is a restaurant website where users can explore the menu, place orders, and pay online seamlessly. It offers a smooth and convenient dining experience from browsing to checkout.",
   },
-   {
-    image: './images/zbeelogo.png',
-    title: 'Opic 3D',
-    link: 'https://github.com/Usman-Javed-07/Opic-3D',
-    displayLink: 'opic3d.com',
+  {
+    image: "./images/zbeelogo.png",
+    title: "character image",
+    link: "https://github.com/Usman-Javed-07/character-image-backend",
+    displayLink: "character-image.com",
     description:
-      'Opic 3D is a mobile app similar to TikTok, allowing users to create, share, and explore engaging short-form 3D videos.'
+      "Character Image is a Shopify store where users can browse and purchase merchandise featuring their favorite characters. It offers a fun and personalized shopping experience for fans of all kinds.",
   },
-    {
-    image: './images/freelancerlogo.png',
-    title: 'Thapa store',
-    link: 'https://github.com/Usman-Javed-07/ecom-project',
-    displayLink: 'thapa.com',
+  {
+    image: "./images/zbeelogo.png",
+    title: "atlas-ai",
+    link: "https://github.com/Usman-Javed-07/atlasAi",
+    displayLink: "atlas-ai.com",
     description:
-      'Thapa is an online store offering mobiles, laptops, and accessories with sections like Products, Explore, and Checkout for a smooth shopping experience.',
+      "This application enables users to interact with an AI through text or voice, choose from a variety of AI personas, and seamlessly send emails via the Gmail API.",
   },
-   {
-    image: './images/freelancerlogo.png',
-    title: 'World Atlas',
-    link: 'https://github.com/Usman-Javed-07/react-project-',
-    displayLink: 'worldatlas.com',
+  {
+    image: "./images/zbeelogo.png",
+    title: "Clothing Store",
+    link: "https://github.com/Usman-Javed-07/clothing-store-backend",
+    displayLink: "ClothingStore.com",
     description:
-      'World Atlas is a project that provides key details of every country, including native name, population, region, capital, currency, domain, and languages.',
+      "Clothing Store is an e-commerce website where users can browse and purchase apparel online, add items to their cart, and complete secure payments with ease.",
   },
-     {
-    image: './images/solslogo.png',
-    title: 'Moshify',
-    link: 'https://github.com/Usman-Javed-07/Html_Css_Design_1',
-    displayLink: 'moshify.com',
+  {
+    image: "./images/zbeelogo.png",
+    title: "Opic 3D",
+    link: "https://github.com/Usman-Javed-07/Opic-3D",
+    displayLink: "opic3d.com",
     description:
-      'Moshify is a simple cloud hosting website built for practice It showcases modern design using HTML and CSS. Great for learning layout, responsiveness, and styling basics.',
+      "Opic 3D is a mobile app similar to TikTok, allowing users to create, share, and explore engaging short-form 3D videos.",
   },
-     {
-    image: './images/solslogo.png',
-    title: 'Industrial supplies',
-    link: 'https://github.com/Usman-Javed-07/Html_Css_Design_2nd',
-    displayLink: 'industrial-supplies.com',
+  {
+    image: "./images/freelancerlogo.png",
+    title: "Thapa store",
+    link: "https://github.com/Usman-Javed-07/ecom-project",
+    displayLink: "thapa.com",
     description:
-      'Industrial Supplies is a comprehensive online platform offering a wide range of industrial products and services. It connects suppliers and buyers, streamlining the procurement process.',
+      "Thapa is an online store offering mobiles, laptops, and accessories with sections like Products, Explore, and Checkout for a smooth shopping experience.",
+  },
+  {
+    image: "./images/freelancerlogo.png",
+    title: "World Atlas",
+    link: "https://github.com/Usman-Javed-07/react-project-",
+    displayLink: "worldatlas.com",
+    description:
+      "World Atlas is a project that provides key details of every country, including native name, population, region, capital, currency, domain, and languages.",
+  },
+  {
+    image: "./images/solslogo.png",
+    title: "Moshify",
+    link: "https://github.com/Usman-Javed-07/Html_Css_Design_1",
+    displayLink: "moshify.com",
+    description:
+      "Moshify is a simple cloud hosting website built for practice It showcases modern design using HTML and CSS. Great for learning layout, responsiveness, and styling basics.",
+  },
+  {
+    image: "./images/solslogo.png",
+    title: "Industrial supplies",
+    link: "https://github.com/Usman-Javed-07/Html_Css_Design_2nd",
+    displayLink: "industrial-supplies.com",
+    description:
+      "Industrial Supplies is a comprehensive online platform offering a wide range of industrial products and services. It connects suppliers and buyers, streamlining the procurement process.",
+  },
+  {
+    image: "./images/solslogo.png",
+    title: "Space Scan 3D",
+    link: "https://github.com/Usman-Javed-07/Html_Css_Design_3",
+    displayLink: "space.scan3d.com",
+    description:
+      "Space Scan 3D is a simple HTML and CSS project designed to showcase a premier 3D virtual tour service provider based in the North Bay, offering immersive and interactive digital experiences.",
+  },
+  {
+    image: "./images/solslogo.png",
+    title: "Figma Land",
+    link: "https://github.com/Usman-Javed-07/html-css-design-4",
+    displayLink: "figmaland.com",
+    description:
+      "Figmaland is a simple HTML and CSS design project inspired by Figma — where the best products begin their journey.",
+  },
+  {
+    image: "./images/freelancerlogo.png",
+    title: "Airline Booking System",
+    link: "https://github.com/Usman-Javed-07/AirlineFrontend",
+    displayLink: "airline.com",
+    description:
+      "Airline is a full-stack web application featuring dedicated panels for users, admins, and employees. Users can book tickets, choose travel classes (Economy, Business, Luxury), select meals, arrange taxis for arrival or departure, and manage luggage. The system supports secure online payments, route management, and ticket cancellation up to one day before the flight.",
   },
 ];
-
 
 const ProjectCard = ({ project }) => (
   <div className="project-card">
@@ -172,7 +195,6 @@ function App() {
   const visibleProjects = showAll ? projectsData : projectsData.slice(0, 4);
 
   return (
-    
     <>
       <header className="navbar">
         <nav className="navbar-container">
@@ -430,40 +452,50 @@ function App() {
       </section>
 
       <section className="projects-section">
-      <div className="projects">
-        <h1 className="about-heading">
-          {" "}
-          <span className="text-about">Projects</span> ( ) {"{"}{" "}
-        </h1>
+        <div className="projects">
+          <h1 className="about-heading">
+            {" "}
+            <span className="text-about">Projects</span> ( ) {"{"}{" "}
+          </h1>
 
-        <div className="work-companies">
-          <p>Some of my notable work</p>
-        </div>
-
-        <section className="projects-container">
-          <div className="project-grid">
-            {visibleProjects.map((project, index) => (
-              <ProjectCard key={index} project={project} />
-            ))}
+          <div className="work-companies">
+            <p>Some of my notable work</p>
           </div>
 
-          {projectsData.length > 4 && (
-            <div style={{ textAlign: 'center' }}>
-              <a
-                onClick={() => setShowAll(!showAll)}
-                className="resume viewmore-btn"
-              >
-                {showAll ? 'Close' : 'View All Projects'}
-                 <span className="arrow">
-                <FaLongArrowAltRight />
-              </span>
-              </a>
+          <section className="projects-container">
+            <div className="project-grid">
+              {visibleProjects.map((project, index) => (
+                <ProjectCard key={index} project={project} />
+              ))}
             </div>
-          )}
-         <p style={{ fontSize: '24px', fontWeight: 500, marginLeft: '50px'}}>{"}"}{" "}</p>
-        </section>
-      </div>
-    </section>
+
+            {projectsData.length > 4 && (
+              <div style={{ textAlign: "center" }}>
+                <a
+                  onClick={() => setShowAll(!showAll)}
+                  className="resume viewmore-btn"
+                >
+                  {showAll ? "Close" : "View All Projects"}
+                  <span className="arrow">
+                    <FaLongArrowAltRight />
+                  </span>
+                </a>
+              </div>
+            )}
+            <p
+              style={{ fontSize: "24px", fontWeight: 500, marginLeft: "50px" }}
+            >
+              {"}"}{" "}
+            </p>
+          </section>
+        </div>
+      </section>
+
+      <section className="my-skills-section">
+        <div className="work-companies">
+          <p>Technologies I have used</p>
+        </div>
+      </section>
     </>
   );
 }
